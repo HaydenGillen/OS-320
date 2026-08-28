1e74cc1cccf7ad9aed6c49c373e34aa767bc3721
haydengillen <gillenh@xavier.edu>
2026-08-27 21:36:32 -0400

    Implement Assignemnt Requirements

--- p0-1.c
+++ p0-1.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+    char c;
+    int alphaCount[26]; 
+} charInfo;
+
+void countCharacters(FILE* text, charInfo* info);
+
 int main(int argc, char *argv[]) {
-    int charCount[26];
-    memset(charCount, 0, sizeof(charCount));
+    charInfo info;
+    memset(info.alphaCount, 0, sizeof(info.alphaCount));
     
     FILE* text = fopen(argv[1], "r");
-    
     if (text == NULL) {
         printf("Error opening %s.\n", argv[1]);
         return 1;
     }
 
-    int c;
-    while ((c = fgetc(text)) != EOF) {
-        if (c >= 'a' && c <= 'z') {
-            charCount[c - 'a']++;
-        } else if (c >= 'A' && c <= 'Z') {
-            charCount[c - 'A']++;
-        }
-    }
-    
+    countCharacters(text, &info);
 
     for (int i = 0; i < 26; i++) {
-        printf("%c: %d\n", 'a' + i, charCount[i]);
+        printf("%c: %d\n", 'A' + i, info.alphaCount[i]);
     }
 
     return 0;
+}
+
+void countCharacters(FILE* text, charInfo* info){
+    while ((info->c = fgetc(text)) != EOF) {
+        if (info->c >= 'a' && info->c <= 'z') {
+            info->alphaCount[info->c - 'a']++;
+        } else if (info->c >= 'A' && info->c <= 'Z') {
+            info->alphaCount[info->c - 'A']++;
+        }
+    }
 }
